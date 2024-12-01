13ddfaa98c3e832e9d169b90317890dc34db30ed
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2024-12-01 11:13:38 +0530

    added pip update in

--- scripts/git_update.sh
+++ scripts/git_update.sh
@@ -3,3 +3,5 @@ rm -rf translation-bot
 sleep 1
 git clone  git@github.com:Ajurahul/translation-bot.git
 echo "$USER : Updated git. bot will restart after this">>/home/ubuntu/logs
+cd translation-bot
+pip install -r requirements.txt
