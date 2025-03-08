64d50f350d0fc10d54a4cad7860d790ed746498f
Ajurahul <48558382+Ajurahul@users.noreply.github.com>
2025-03-08 15:33:57 +0530

    all changes for fast speed

--- scripts/server-restart.sh
+++ scripts/server-restart.sh
@@ -1,4 +1,4 @@
 nowtime=$(date)
 echo "$USER : Restarting server at $nowtime">>/home/ec2-user/logs
-aws ec2 reboot-instances --instance-ids i-025c10c1ed58f97b3 --profile admin >>/home/ec2-user/logs
+aws ec2 reboot-instances --instance-ids i-00d5a946cd73a5150 --profile admin >>/home/ec2-user/logs
 sudo systemctl reboot
